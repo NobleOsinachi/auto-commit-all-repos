a00ac238e82bb1720e4a1af8bbbcce58db3e99cc
Noble Osinachi <nobleosinachi98@gmail.com>
2023-04-14 22:19:30 +0100

    Refactor dateString function to use native JS Date functions for improved readability

--- index.js
+++ index.js
@@ -56,27 +56,8 @@ fs.readdir(parentDirectoryPath, { withFileTypes: true }, (err, files) => {
     });
 });
 
-
 function dateString() {
     const date = new Date();
-
-    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const monthsOfYear = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-    const dayOfWeek = daysOfWeek[date.getDay()];
-    const monthOfYear = monthsOfYear[date.getMonth()];
-    const dayOfMonth = date.getDate();
-    const year = date.getFullYear();
-    let hours = date.getHours();
-    const minutes = date.getMinutes().toString().padStart(2, '0');
-    const seconds = date.getSeconds().toString().padStart(2, '0');
-
-    const meridiem = hours < 12 ? 'AM' : 'PM';
-
-    hours = hours % 12;
-    hours = hours ? hours : 12;
-
-    const dateString = `${dayOfWeek} ${monthOfYear} ${dayOfMonth} ${year} at ${hours.toString().padStart(2, '0')}:${minutes}:${seconds} ${meridiem}`;
-
-    return dateString;
+    const options = { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true };
+    return date.toLocaleDateString('en-US', options);
 }
