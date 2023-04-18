4e505b984de8bf6b68406eb9c46db89c1bb70e0f
Noble Osinachi <nobleosinachi98@gmail.com>
2023-04-18 15:20:36 +0100

    Implement commit message generator

---
+++ message.js
@@ -0,0 +1,19 @@
+const commits = [];
+
+const actions = ['fix', 'update', 'refactor', 'improve', 'optimize', 'revert'];
+const descriptions = ['bug', 'performance', 'security', 'style', 'typo', 'logic'];
+const adjectives = ['duplicate', 'error', 'issue', 'problem', 'conflict', 'mistake'];
+const words = ['fix', 'update', 'add', 'remove', 'refactor', 'merge', 'implement', 'optimize', 'reorganize', 'clean']; const nouns = ['bug', 'feature', 'code', 'function', 'variable', 'library', 'module', 'algorithm', 'interface', 'database'];
+
+for (let i = 0; i < 50; i++) {
+    const action = actions[Math.floor(Math.random() * actions.length)];
+    const description = descriptions[Math.floor(Math.random() * descriptions.length)];
+    const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+    const randomWord = words[Math.floor(Math.random() * words.length)];
+    const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+    const randomNumber = Math.floor(Math.random() * 100);
+    const message = `${description} ${action}: ${randomWord} ${adjective} ${randomNoun} #${randomNumber}`;
+    commits.push(message);
+}
+
+console.log(commits);
